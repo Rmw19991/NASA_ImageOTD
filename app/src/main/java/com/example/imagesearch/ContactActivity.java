9d4449e18c68de8f616a1bbdf17b83cf2d535134
Roy <roymw09@gmail.com>
2021-03-26 18:47:26 -0230

    Milestone 2 - added send email functionality

--- app/src/main/java/com/example/imagesearch/ContactActivity.java
+++ app/src/main/java/com/example/imagesearch/ContactActivity.java
@@ -1,7 +1,9 @@
 package com.example.imagesearch;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class ContactActivity extends ToolbarActivity
 {
@@ -16,5 +18,25 @@ public class ContactActivity extends ToolbarActivity
 
         // load toolbar
         loadToolbar(getString(R.string.navTitle_Contact), VERSION);
+
+    }
+
+    // Signs in to the users Gmail account and sends an email
+    public void sendEmail(View v)
+    {
+        EditText senderText = findViewById(R.id.contact_NameEditText);
+        EditText messageText = findViewById(R.id.contact_MessageEditText);
+        EditText subjectText = findViewById(R.id.contact_SubjectEditText);
+        String subject = subjectText.getText().toString();
+        String sender = senderText.getText().toString();
+        String message = messageText.getText().toString();
+
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[] {"destination@emailhere.com"}); // USERS EMAIL WILL BE SENT TO THIS EMAIL ADDRESS
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "From: " + sender + "\n\n" + message);
+
+        startActivity(Intent.createChooser(emailIntent, "GMAIL"));
     }
 }
